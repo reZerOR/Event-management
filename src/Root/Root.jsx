d69d428ea648bac1bac41d0db9cbe7320bbd562b
reZerOR <gkraihan14k@gmail.com>
2023-10-07 22:50:10 +0600

    project file and fake data created

---
+++ src/Root/Root.jsx
@@ -0,0 +1,13 @@
+import { Outlet } from "react-router-dom";
+import Navber from "../Components/Navber/Navber";
+
+const Root = () => {
+  return (
+    <div>
+      <Navber></Navber>
+      <Outlet></Outlet>
+    </div>
+  );
+};
+
+export default Root;
---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,9 @@
+const Home = () => {
+  return (
+    <div>
+      <p className="text-2xl">this is home</p>
+    </div>
+  );
+};
+
+export default Home;
